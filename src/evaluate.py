1fa3d2fa13db3fad5c3f07dda1dc6813998d4d3f
Aobo Yang <yangaobo@gmail.com>
2019-04-25 18:02:29 -0400

    use sigmoid with 1 channel in model instead of softmax

--- src/evaluate.py
+++ src/evaluate.py
@@ -9,17 +9,16 @@ import random
 import torch
 from torch.nn import functional as F
 from torch.utils.data import DataLoader
-from torch.nn.functional import mse_loss
 import matplotlib.pyplot as plt
 
 import config
-from loss import iou_loss, hairmat_loss, acc_loss, F1_loss
+from loss import iou_loss, HairMattingLoss, acc_loss, F1_loss
 from utils import create_multi_figure
 
-
 USE_CUDA = torch.cuda.is_available()
-#DEVICE = torch.device("cuda" if USE_CUDA else "cpu")
-DEVICE = torch.device('cpu')
+DEVICE = torch.device("cuda" if USE_CUDA else "cpu")
+
+hairmat_loss = HairMattingLoss(config.GRAD_LOSS_LAMBDA)
 
 def evalTest(test_data, model):
     testloader = DataLoader(test_data, batch_size=4, shuffle=False)
@@ -29,7 +28,7 @@ def evalTest(test_data, model):
         image, mask = (i.to(DEVICE) for i in batch)
 
         pred = model(image)
-        total_loss += hairmat_loss(pred, image, mask).item()
+        total_loss += hairmat_loss(pred, mask, image).item()
         iloss = iou_loss(pred, mask).item()
         total_iou += iloss
         aloss = acc_loss(pred, mask).item()
@@ -46,18 +45,13 @@ def evaluateOne(img, model, absolute=True):
     img = img.to(DEVICE).unsqueeze(0)
     pred = model(img)
 
-    pred = pred.squeeze() # remove batch dim
-
     if absolute:
-        pred = pred.argmax(0)
+        pred[pred > .5] = 1.
+        pred[pred <= .5] = 0.
     else:
-        pred = F.softmax(pred, dim=0)
-        pred = pred[1]
         pred[pred < .4] = 0
 
-    pred = pred.float()
-
-    rows = [[img[0], pred]]
+    rows = [[img[0], pred[0]]]
     create_multi_figure(rows, dye=True)
     plt.show()
 
@@ -66,25 +60,27 @@ def evaluate(test_data, model, num, absolute=True):
     rows = [None] * num
     for i in range(num):
         idx = random.randint(0, len(test_data) - 1)
-    # for i, idx in enumerate([122, 184, 106, 202]):
+    # for i, idx in enumerate([
+    #     203, 162, 53, 116, 159, 153, 140, 37, 184, 106, 133
+    # ]):
+
+    # for i, idx in enumerate([
+    #     58, 151, 85, 34, 122, 119, 8, 144, 202, 154, 88
+    # ]):
         image, mask = (i.to(DEVICE).unsqueeze(0) for i in test_data[idx])
         pred = model(image)
 
-        celoss, igloss = hairmat_loss(pred, image, mask)
-        print(idx, 'ce loss:', celoss.item(), 'ig loss:', igloss.item())
+        loss = hairmat_loss(pred, mask, image)
+        print(idx, 'loss:', loss.item())
 
-        pred = pred.squeeze() # remove batch dim
 
         if absolute:
-            pred = pred.argmax(0)
+            pred[pred > .5] = 1.
+            pred[pred <= .5] = 0.
         else:
-            pred = F.softmax(pred, dim=0)
-            pred = pred[1]
             pred[pred < .4] = 0
 
-        pred = pred.float()
-
-        rows[i] = [image[0], mask[0], pred]
+        rows[i] = [image[0], mask[0], pred[0]] # get batch
 
     create_multi_figure(rows, dye=True)
     plt.show()
--- src/loss.py
+++ src/loss.py
@@ -2,7 +2,7 @@ from skimage.color import rgb2gray
 from skimage import filters
 from sklearn.preprocessing import normalize
 import torch
-
+from torch import nn
 from torch.nn.modules.loss import _WeightedLoss
 import torch.nn.functional as F
 import config
@@ -10,38 +10,81 @@ import config
 USE_CUDA = torch.cuda.is_available()
 DEVICE = torch.device("cuda" if USE_CUDA else "cpu")
 
-def image_gradient(image):
-  edges_x = filters.sobel_h(image)
-  edges_y = filters.sobel_v(image)
-  edges_x = normalize(edges_x)
-  edges_y = normalize(edges_y)
-  return torch.from_numpy(edges_x), torch.from_numpy(edges_y)
+class HairMattingLoss(nn.modules.loss._Loss):
+  def __init__(self, ratio_of_Gradient=0.0):
+    super(HairMattingLoss, self).__init__()
+    self.ratio_of_gradient = ratio_of_Gradient
+    self.bce_loss = nn.BCELoss()
+
+    if self.ratio_of_gradient > 0:
+      sobel_kernel_x = torch.Tensor([
+        [1.0, 0.0, -1.0],
+        [2.0, 0.0, -2.0],
+        [1.0, 0.0, -1.0]
+      ])
+      self.sobel_kernel_x = sobel_kernel_x.view(1,1,3,3)
+
+      sobel_kernel_y = torch.Tensor([
+        [1.0, 2.0, 1.0],
+        [0.0, 0.0, 0.0],
+        [-1.0, -2.0, -1.0]
+      ])
+      self.sobel_kernel_y = sobel_kernel_y.view(1,1,3,3)
+
+  def forward(self, pred, mask, image):
+    loss = self.bce_loss(pred, mask)
+
+    if self.ratio_of_gradient > 0:
+      I_x = F.conv2d(image, sobel_kernel_x)
+      G_x = F.conv2d(pred, sobel_kernel_x)
+
+      I_y = F.conv2d(image, sobel_kernel_y)
+      G_y = F.conv2d(pred, sobel_kernel_y)
+
+      G = torch.sqrt(G_x.pow(2) + G_y.pow(2))
+
+      rang_grad = 1 - torch.pow(I_x * G_x + I_y * G_y, 2)
+      # rang_grad = rang_grad if rang_grad > 0 else 0
+
+      loss2 = torch.sum(G * rang_grad) / torch.sum(G) + 1e-6
+
+      loss = loss + loss2 * self.ratio_of_gradient
+
+    return loss
 
 
-def image_gradient_loss(image, pred):
-  loss = 0
-  for i in range(len(image)):
-    pred_grad_x, pred_grad_y = image_gradient(pred[i][0].cpu().detach().numpy())
-    gray_image = torch.from_numpy(rgb2gray(image[i].permute(1, 2, 0).cpu().numpy()))
-    image_grad_x, image_grad_y = image_gradient(gray_image)
-    IMx = (image_grad_x * pred_grad_x).float()
-    IMy = (image_grad_y * pred_grad_y).float()
-    Mmag = (pred_grad_x.pow(2) + pred_grad_y.pow(2)).sqrt().float()
-    IM = 1 - (IMx + IMy).pow(2)
-    numerator = torch.sum(Mmag * IM)
-    denominator = torch.sum(Mmag)
-    loss = loss + numerator / denominator
-
-  return torch.div(loss, len(image))
+# def image_gradient(image):
+#   edges_x = filters.sobel_h(image)
+#   edges_y = filters.sobel_v(image)
+#   edges_x = normalize(edges_x)
+#   edges_y = normalize(edges_y)
+#   return torch.from_numpy(edges_x), torch.from_numpy(edges_y)
 
 
-def hairmat_loss(pred, image, mask):
-  pred_flat = pred.permute(0, 2, 3, 1).contiguous().view(-1, 2)
-  mask_flat = mask.squeeze(1).view(-1).long()
-  cross_entropy_loss = F.cross_entropy(pred_flat, mask_flat)
-  # image_loss = image_gradient_loss(image, pred).to(DEVICE)
-  image_loss = 0
-  return cross_entropy_loss, image_loss.float()
+# def image_gradient_loss(image, pred):
+#   loss = 0
+#   for i in range(len(image)):
+#     pred_grad_x, pred_grad_y = image_gradient(pred[i][0].cpu().detach().numpy())
+#     gray_image = torch.from_numpy(rgb2gray(image[i].permute(1, 2, 0).cpu().numpy()))
+#     image_grad_x, image_grad_y = image_gradient(gray_image)
+#     IMx = (image_grad_x * pred_grad_x).float()
+#     IMy = (image_grad_y * pred_grad_y).float()
+#     Mmag = (pred_grad_x.pow(2) + pred_grad_y.pow(2)).sqrt().float()
+#     IM = 1 - (IMx + IMy).pow(2)
+#     numerator = torch.sum(Mmag * IM)
+#     denominator = torch.sum(Mmag)
+#     loss = loss + numerator / denominator
+
+#   return torch.div(loss, len(image))
+
+
+# def hairmat_loss(pred, image, mask):
+#   pred_flat = pred.permute(0, 2, 3, 1).contiguous().view(-1, 2)
+#   mask_flat = mask.squeeze(1).view(-1).long()
+#   cross_entropy_loss = F.cross_entropy(pred_flat, mask_flat)
+#   # image_loss = image_gradient_loss(image, pred).to(DEVICE).float()
+#   image_loss = torch.tensor(0).float()
+#   return cross_entropy_loss, image_loss
 
 def iou_loss(pred, mask):
   pred = torch.argmax(pred, 1).long()
